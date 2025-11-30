96435a8e68e72e67be8dd7cef8494df3c9324bce
Henry Aleksanian (Shok.) <henula09106@gmail.com>
2025-11-30 14:41:44 +0400

    fix, refactor: oauthLogin

--- src/services/oauth/oauthLogin.service.ts
+++ src/services/oauth/oauthLogin.service.ts
@@ -1,4 +1,4 @@
-import axios, { AxiosError } from 'axios';
+import axios from 'axios';
 import prisma from "src/utils/prismaClient.js";
 import generateJwtTokens from 'src/utils/generateJwtTokens.js';
 import { v4 as uuidv4 } from 'uuid';
@@ -8,24 +8,30 @@ import { USER_SERVICE_URL } from 'src/utils/env.js';
 const oauthLogin = (githubOAuth2: any) => {
 	return {
 		async getUserData(request: any) {
+			let profile: any;
+			let emails: any;
 
 			const tokenData = await githubOAuth2.getAccessTokenFromAuthorizationCodeFlow(request);
 			const { access_token } = tokenData.token;
 
 			if (!access_token) throw new AppError('NO_OAUTH_TOKEN');
 
-			const profile = await axios.get('https://api.github.com/user', {
-				headers: { Authorization: `token ${access_token}` },
-			});
+			try {
 
-			const emails = await axios.get('https://api.github.com/user/emails', {
-				headers: { Authorization: `token ${access_token}` },
-			})
+				profile = await axios.get('https://api.github.com/user', {
+					headers: { Authorization: `token ${access_token}` },
+				});
 
-			if (profile.status !== 200 || emails.status !== 200) throw new AppError('GITHUB_API_ERROR');
+				emails = await axios.get('https://api.github.com/user/emails', {
+					headers: { Authorization: `token ${access_token}` },
+				});
+
+			} catch (error) {
+				throw new AppError('GITHUB_API_ERROR');
+			}
 
 			const profileLogin = profile.data.login;
-			if (!profileLogin) throw new AppError('NO_GITHUB_USERNAME'); // controller catch
+			if (!profileLogin) throw new AppError('NO_GITHUB_USERNAME');
 
 			const primaryEmail = emails.data.find((e: any) => e.primary && e.verified)?.email;
 			if (!primaryEmail) throw new AppError('NO_VERIFIED_EMAIL');
@@ -44,39 +50,47 @@ const oauthLogin = (githubOAuth2: any) => {
 					},
 				});
 
-				const username = `user${uuidv4().slice(0, 8)}`;
-				const result = await axios.post(`${USER_SERVICE_URL}/`,
-					{
-						'userId': user.id,
-						username
-					},
-					{
-						headers: {
-							'x-service-token': process.env.SERVICE_TOKEN,
-							'Content-Type': 'application/json',
-						}
-					});
+				let username = `user${uuidv4().slice(0, 8)}`;
+				try {
+					await axios.post(`${USER_SERVICE_URL}/`,
+						{
+							'userId': user.id,
+							username
+						},
+						{
+							headers: {
+								'x-service-token': process.env.SERVICE_TOKEN,
+								'Content-Type': 'application/json',
+							}
+						});
+				}
+				catch (error) {
+					if (error?.response?.status === 409) {
 
-			} catch (error) {
-				if (error instanceof AxiosError && error.response?.status === 409) {
-					username = `user${uuidv4().slice(0, 8)}`;
-					await axios.post(`${USER_SERVICE_URL}/`, {
-						'userId': user.id,
-						username
-					});
+						username = `user${uuidv4().slice(0, 8)}`;
+
+						await axios.post(`${USER_SERVICE_URL}/`, {
+							userId: user.id,
+							username
+						}, {
+							headers: {
+								'x-service-token': process.env.SERVICE_TOKEN,
+								'Content-Type': 'application/json',
+							}
+						});
+
+					} else {
+						await prisma.authUser.delete({ where: { id: user.id } });
+						throw new AppError('USER_SERVICE_ERROR');
+					}
 				}
-				else {
-					await prisma.authUser.delete({ where: { id: user.id } });
-					throw new AppError('USER_SERVICE_ERROR');
-				}
+
 			}
-		}
-
 			const tokens = await generateJwtTokens(user.id);
 
-		return { userId: user.id, ...tokens };
-	},
-	};
-}
+			return { userId: user.id, ...tokens };
+		}
+	}
+};
 
 export default oauthLogin;
